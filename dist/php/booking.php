4103cf31791c5a534f40fa818167aedf8d458b1d
Viktorija Turjak <viktorija.turjak26@gmail.com>
2020-10-20 17:08:16 +0200

    Contact forms

---
+++ dist/php/booking.php
@@ -0,0 +1,22 @@
+<?php
+
+if (isset($_POST['submit'])) {
+    $checkin = $_POST['checkin'];
+    $checkout = $_POST['checkout'];
+    $number= $_POST['number'];
+    $children= $_POST['children'];
+    $room = $_POST['room'];
+    $roomnumber= $_POST['roomnumber'];
+
+    $mailTo = "viktorija.turjak2612@gmail.com";
+    $subject = "New reservation";
+    $headers = "From ".$checkin."\r\n";
+    $headers .= "To ".$checkout."\r\n";
+    $headers .= "Adults: ".$number."\r\n";
+    $headers .= "Children: ".$children."\r\n";
+    $headers .= "Room: ".$room."\r\n";
+    $headers .= "Room number: " .$roomnumber;
+
+    mail($mailTo, $subject, $headers);
+    header("Location: index.html?mailsend");
+}
---
+++ dist/php/contactform.php
@@ -0,0 +1,15 @@
+<?php
+
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $lastmame = $_POST['lastname'];
+    $mailFrom = $_POST['mail'];
+    $message= $_POST['message'];
+
+    $mailTo = "viktorija.turjak2612@gmail.com";
+    $headers = "From: ".$mailFrom;
+    $txt = "You have received an e-mail from ".$name.".\n\n".$$message;
+
+    mail($mailTo, $txt, $headers, $message);
+    header("Location: index.html?mailsend");
+}
